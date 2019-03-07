c2e1a3f15e5d879dffe5303087929226a55a93a8
jairo <jairovengoechea@live.com>
2019-03-08 00:07:35 +0100

    Adding scopes

--- EntityProvider/EP.cs
+++ EntityProvider/EP.cs
@@ -11,8 +11,51 @@ namespace EntityProvider
         ///     Entity Provider singleton instance
         /// </summary>
         private static EP _instance = null;
+        
+        /// <summary>
+        ///     Path to implementation dll
+        /// </summary>
         private string _dllLocation;
+        
+        /// <summary>
+        ///     Implementations namespace
+        /// </summary>
         private string _implementationsNamespace;
+
+        /// <summary>
+        ///     Collection that stores scoped objects
+        /// </summary>
+        private readonly IDictionary<Type, object> _scoped = new Dictionary<Type, object>();
+
+        /// <summary>
+        ///     Collection that stores singleton objects
+        /// </summary>
+        private static readonly IDictionary<Type, object> Singletons = new Dictionary<Type, object>();
+
+        /// <summary>
+        ///     Returns an instance of the passed ScopeType
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="s"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        private T Get<T>(ScopeType s = ScopeType.Transient, params object[] args)
+        {
+            // Return new instance if transient
+            if(s == ScopeType.Transient) return New<T>(args);
+            // Select collection
+            var collection = s == ScopeType.Scoped
+                ? _scoped
+                : Singletons;
+            // Find Object
+            if (collection.ContainsKey(typeof(T))) return (T) collection[typeof(T)];
+            var obj = New<T>(args);
+            // Add to collection of not exists
+            if(s != ScopeType.Transient) collection.Add(typeof(T), obj);
+            // Return object
+            return obj;
+        }
+
         /// <summary>
         ///     Entity provider private constructor
         /// </summary>
@@ -32,16 +75,49 @@ namespace EntityProvider
         }
 
         /// <summary>
-        ///     Returns fresh instance of an object
+        ///     Returns an instance of the Implementation
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        private T New<T>(params object[] args)
+        {
+            var wantedType = typeof(T);
+            var modelType = GetModelTypeOf(wantedType);
+            return (T)Activator.CreateInstance(modelType, args);
+        }
+
+        /// <summary>
+        ///     Returns a scoped instance of the object
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public T GetScoped<T>(params object[] args)
+        {
+            return Get<T>(ScopeType.Scoped, args);
+        }
+
+        /// <summary>
+        ///     Returns a singleton instance of the object
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public T GetSingleton<T>(params object[] args)
+        {
+            return Get<T>(ScopeType.Singleton, args);
+        }
+
+        /// <summary>
+        /// Returns a transient instance
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="args"></param>
         /// <returns></returns>
         public T GetTransient<T>(params object[] args)
         {
-            var wantedType = typeof(T);
-            var modelType = GetModelTypeOf(wantedType);
-            return (T) Activator.CreateInstance(modelType, args);
+            return Get<T>(args: args);
         }
 
         /// <summary>
@@ -78,5 +154,14 @@ namespace EntityProvider
                       .Where(t => string.Equals(t.Namespace, nameSpace, StringComparison.Ordinal))
                       .ToArray();
         }
+
+    
+        private enum ScopeType
+        {
+            Transient,
+            Scoped,
+            Singleton
+        }
+
     }
 }
